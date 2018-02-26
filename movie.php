f6b05b2073b3168b3a6d234ea386435b0f96dcb2
Vishal Panchal <vp296@njit.edu>
2018-02-26 18:45:40 -0500

    added new api file

---
+++ movie.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<form action="movie.php" method="POST">
+ <br>
+ <label>Movie Name</label>
+  <input type="text" name="name" value="">
+  <br>
+ 
+  
+  
+  <br>
+  
+  <input type="submit" value="Submit">
+</form>
+
+</html>
+
+<?php
+//step1
+
+
+$cSession = curl_init(); 
+//step2
+$name = $_POST['name'];
+
+$url = "https://api.themoviedb.org/3/search/movie?api_key=63c5f394ee102de7ba90c4a6159080c2&query='".$name."'";
+$url = str_replace(" ", '%20', $url);
+curl_setopt($cSession,CURLOPT_URL, $url);
+curl_setopt($cSession,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($cSession,CURLOPT_HEADER, false); 
+//step3
+$result=curl_exec($cSession);
+//step4
+curl_close($cSession);
+//step5
+//echo $result;
+
+$data =  json_decode($result,true);
+
+//print_r($data);
+
+
+
+
+foreach ($data['results'][0] as $key =>$value) {
+	
+	
+	echo "<br>";
+	$moviedata = array();
+	$moviedata = array("$key"=>"$value");
+	
+	echo $moviedata['id'];
+	echo $moviedata['title'];
+	echo $moviedata['overview'];
+					if($key == 'genre_ids'){
+						
+					$genres = array();	
+				foreach ($data['results'][0]['genre_ids'] as $key =>$value) {
+						//echo "$value";
+						$genres[] = $value;
+										
+				}
+				//print_r($genres);
+					echo "genre_ids: ";echo implode(', ', $genres);					
+					
+					}
+	
+	echo $moviedata['release_date'];
+	echo $moviedata['poster_path'];
+	echo $moviedata['backdrop_path'];
+	 
+}
+
+?>
