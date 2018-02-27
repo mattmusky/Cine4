225c6d5178853bdc260535dee3d536cea3f23864
mattmusky <mattmusky@gmail.com>
2018-02-26 19:11:30 -0500

    simplify

--- src/api/movie.php
+++ src/api/movie.php
@@ -3,7 +3,7 @@
 //step1
 
 
-$cSession = curl_init(); 
+$cSession = curl_init();
 //step2
 $name = "cars";
 
@@ -11,7 +11,7 @@ $url = "https://api.themoviedb.org/3/search/movie?api_key=63c5f394ee102de7ba90c4
 $url = str_replace(" ", '%20', $url);
 curl_setopt($cSession,CURLOPT_URL, $url);
 curl_setopt($cSession,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($cSession,CURLOPT_HEADER, false); 
+curl_setopt($cSession,CURLOPT_HEADER, false);
 //step3
 $result=curl_exec($cSession);
 //step4
@@ -24,34 +24,36 @@ $data =  json_decode($result,true);
 //print_r($data);
 
 
+echo $data['results'][0]['title'];
 
-
+/*
 foreach ($data['results'][0] as $key =>$value) {
-	
-	
+
+
 	echo "<br>";
 	$moviedata = array();
 	$moviedata = array("$key"=>"$value");
-	
+
 	echo $moviedata['id'];
 	echo $moviedata['title'];
 	echo $moviedata['overview'];
 					if($key == 'genre_ids'){
-						
-					$genres = array();	
+
+					$genres = array();
 				foreach ($data['results'][0]['genre_ids'] as $key =>$value) {
 						//echo "$value";
 						$genres[] = $value;
-										
+
 				}
 				//print_r($genres);
-					echo "genre_ids: ";echo implode(', ', $genres);					
-					
+					echo "genre_ids: ";echo implode(', ', $genres);
+
 					}
-	
+
 	echo $moviedata['release_date'];
 	echo $moviedata['poster_path'];
-	echo $moviedata['backdrop_path'];	 
+	echo $moviedata['backdrop_path'];
+*/
 }
 
 ?>
