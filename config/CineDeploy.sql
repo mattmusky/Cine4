f0f0472a4000faba4d420fde8b665315b5a9eb92
jxs4 <jxs94@yahoo.com>
2018-04-20 12:24:49 -0400

    deploy db changes

--- config/CineDeploy.sql
+++ config/CineDeploy.sql
@@ -40,18 +40,18 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `CineDeploy`.`Version` (
   `VersionID` INT(10) NOT NULL AUTO_INCREMENT,
   `VersionNum` VARCHAR(45) NULL,
-  `Date` DATETIME NULL,
+  `Createtime` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `Deprecate` ENUM('Y', 'N') NULL DEFAULT 'N',
-  `PackageName` VARCHAR(45) NULL,
-  PRIMARY KEY (`VersionID`),
-  CONSTRAINT `Version.PackageName`
+  `PackageName` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`VersionID`, `PackageName`),
+  CONSTRAINT `Version_Package_PackageName`
     FOREIGN KEY (`PackageName`)
     REFERENCES `CineDeploy`.`Package` (`PackageName`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `Version.PackageName_idx` ON `CineDeploy`.`Version` (`PackageName` ASC);
+CREATE INDEX `Version_Package_PackageName_idx` ON `CineDeploy`.`Version` (`PackageName` ASC);
 
 
 -- -----------------------------------------------------
@@ -60,7 +60,8 @@ CREATE INDEX `Version.PackageName_idx` ON `CineDeploy`.`Version` (`PackageName` 
 CREATE TABLE IF NOT EXISTS `CineDeploy`.`Machinehaspackage` (
   `HostIP` VARCHAR(100) NOT NULL,
   `VersionID` INT(10) NOT NULL,
-  `Date` DATETIME NULL,
+  `UpdateTime` DATETIME NULL,
+  `PackageName` VARCHAR(45) NULL,
   PRIMARY KEY (`HostIP`, `VersionID`),
   CONSTRAINT `Machinehaspackage_Machine_Hostname`
     FOREIGN KEY (`HostIP`)
@@ -76,8 +77,18 @@ ENGINE = InnoDB;
 
 CREATE INDEX `Machinehaspackage_Version_VersionID_idx` ON `CineDeploy`.`Machinehaspackage` (`VersionID` ASC);
 
+USE `CineDeploy`;
+
+DELIMITER $$
+USE `CineDeploy`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `CineDeploy`.`Machinehaspackage_BEFORE_UPDATE` BEFORE UPDATE ON `Machinehaspackage` FOR EACH ROW
+BEGIN
+	SET NEW.UpdateTime = CURTIME();
+END$$
+
+
+DELIMITER ;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
