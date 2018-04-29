4d44ca1d42643680ef8955180c18929c67b00550
Matt Muscarella <mattmusky@gmail.com>
2018-04-29 12:45:08 -0400

    dep up 5

--- deploy/depBack.php
+++ deploy/depBack.php
@@ -7,8 +7,8 @@ require_once('rmq/rabbitMQPush.inc');
 function push($package, $target, $machine)
 {
   $host = preg_split('[-]', gethostname());
-  $host = (strtolower($host[1] . $host[2]));
-  if ($host == $target . $machine) {
+  $thishost = (strtolower($host[1] . $host[2]));
+  if ($thishost == $target . $machine) {
     print "\n"; echo ("Deploying...");
 
   switch ($package) {
@@ -21,7 +21,7 @@ function push($package, $target, $machine)
       break;
     }
     case ("db"): {
-      if (!$host[3]=='HSB'){
+      if (!isset($host[3])){
       print "\n"; echo ("tar -xf /home/cine/pack/box.tar -C /");
       print "\n"; echo ("mysql -u root -pinfinity cine4 < /home/cine/cine4/config/cine4.sql");
     }
@@ -29,14 +29,14 @@ function push($package, $target, $machine)
     }
     case ("bephp"): {
       print "\n"; echo ("tar -xf /home/cine/pack/box.tar -C /");
-      if (!$host[3]) {
+      if (!isset($host[3])) {
       print "\n"; echo ("systemctl restart cine4_auth cine4_data cine4_log");
     }
       break;
     }
     case ("apiphp"): {
       print "\n"; echo ("tar -xf /home/cine/pack/box.tar -C /");
-      if (!$host[3]) {
+      if (!isset($host[3])) {
       print "\n"; echo ("systemctl restart cine4_api");
     }
       break;
