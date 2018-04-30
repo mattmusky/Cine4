a039bc8c389c5d39c01c6ab36f88fd0952a497b6
Matt Muscarella <mattmusky@gmail.com>
2018-04-30 11:08:19 -0400

    dep sql

--- config/deploy.sql
+++ config/deploy.sql
@@ -1,14 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
 --
--- Host: localhost
--- Generation Time: Apr 29, 2018 at 08:06 PM
--- Server version: 5.7.21-0ubuntu0.16.04.1
--- PHP Version: 7.0.28-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+-- Host: localhost    Database: CineDeploy
+-- ------------------------------------------------------
+-- Server version	5.7.21-0ubuntu0.16.04.1
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
