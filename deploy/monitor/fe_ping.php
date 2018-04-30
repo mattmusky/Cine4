26fc046cdafa886dc972d333d511c71d45952c26
Matt Muscarella <mattmusky@gmail.com>
2018-04-30 15:44:13 -0400

    fe fail

---
+++ deploy/monitor/fe_ping.php
@@ -0,0 +1,20 @@
+<?php
+$host = '10.2.2.12';
+//PHP script to ping remote server and edit configs to enable HSB
+function ping($host)
+{
+  exec(sprintf('ping -c 1 -W 10 %s', escapeshellarg($host)), $res, $rval);
+  return $rval === 0;
+}
+while (1) {
+  sleep(10);
+  $up = ping($host);
+  if ($up) {
+    //echo 'UP';
+  } else {
+    echo 'Down';
+    exec("mv /home/cine/cine4/public/php/rmq/apiMQ.ini /home/cine/cine4/public/php/rmq/apiMQ.bak");
+    exec("mv /home/cine/cine4/public/php/rmq/apiMQ.fail /home/cine/cine4/public/php/rmq/apiMQ.ini");
+    exit();
+  }
+}
